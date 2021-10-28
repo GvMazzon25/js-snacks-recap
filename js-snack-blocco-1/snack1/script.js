63b09714859606a7395f93c5947d02289db0ff12
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-28 15:23:50 +0200

    end-script1-blocco1

--- js-snack-blocco-1/snack1/script.js
+++ js-snack-blocco-1/snack1/script.js
@@ -6,9 +6,9 @@ const secondWord = prompt('Digita la seconda parola')
 
 
 if(firstWord.length > secondWord.length){
-    console.log(`${firstWord} ${secondWord}`);
+    console.log(`${firstWord} è più lunga di ${secondWord}`);
 }else if(firstWord.length < secondWord.length){
-    console.log(`${secondWord} ${firstWord}`)
+    console.log(`${secondWord} è più lunga di ${firstWord}`)
 }else{
     console.log('Le due parole hanno lo stesso numero di caratteri')
 }

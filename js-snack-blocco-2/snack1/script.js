c60875322c6547a98d8f1b6d4b91fb2ce8663293
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-28 15:51:25 +0200

    end-snack1-blocco2

--- js-snack-blocco-2/snack1/script.js
+++ js-snack-blocco-2/snack1/script.js
@@ -1,3 +1,13 @@
 //Snack 1 Blocco 2
 
 //Variabile
+let num = parseInt(prompt('Inserisci un numero'));
+let dispariPiu1 = ('')
+
+if (num % 2 === 0){
+    alert(`Il numero ${num} è pari`);
+    console.log(num)
+}else{
+    //dispariPiu1 = num + 1;
+    alert(`Il numero ${num} è dispari, il numero pari successivo è ${num += 1}`)
+}

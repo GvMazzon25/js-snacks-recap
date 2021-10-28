4c2352fc1eb026189518081da7f6545da28bebe3
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-28 15:59:49 +0200

    array-list

---
+++ js-snack-blocco-2/snack2/script.js
@@ -0,0 +1,12 @@
+//Snack 2 Blocco 2
+
+//Variabili e Array
+
+const nameList = ['Giorgio','Martina','Giuseppe','Davide','Massimo','Federica','Alessandra'];
+const surnameList = ['Verdi','Russo','Rossi','Pesarin','Crepaldi','Di Trapani','Avellone'];
+
+const invitedList = [''];
+
+
+
+

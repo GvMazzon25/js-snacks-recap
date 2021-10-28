e32681ea2f7a66a4e4873401fa74d1d88fd97a31
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-28 16:43:31 +0200

    after

--- js-snack-blocco-2/snack2/script.js
+++ js-snack-blocco-2/snack2/script.js
@@ -2,11 +2,26 @@
 
 //Variabili e Array
 
-const nameList = ['Giorgio','Martina','Giuseppe','Davide','Massimo','Federica','Alessandra'];
+const nameList = ['Giorgio ','Martina ','Giuseppe ','Davide ','Massimo ','Federica ','Alessandra '];
 const surnameList = ['Verdi','Russo','Rossi','Pesarin','Crepaldi','Di Trapani','Avellone'];
 
-const invitedList = [''];
+let invitedList = [''];
 
+let completeName =('')
 
+const startBtn = document.querySelector('.btn');
+
+startBtn.addEventListener('click', function() {
+    num1 = Math.round(Math.random() * 7);
+    console.log(num1);
+    num2 = Math.round(Math.random() * 7);
+    console.log(num2)
+
+    completeName = nameList[num1] + '' + surnameList[num2];
+    invitedList.push(completeName);
+    console.log(completeName);
+})
+
+alert(`Hai invitato ${completeName}`);
 
 

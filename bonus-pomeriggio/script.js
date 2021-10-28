def8f4303fd29ce0845d9077c382747a22595c10
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-28 16:56:39 +0200

    click

---
+++ bonus-pomeriggio/script.js
@@ -0,0 +1,17 @@
+
+/** Ogni volta che si clicca un bottone, il nostro programma chiederà un numero di 4 cifre all’utente.
+Se l’utente ha inserito un numero corretto, allora il programma calcolerà la somma e le media di tutte 
+le cifre che lo compongono stampandola nel DOM, altrimenti ripeterà la richiesta di inserimento finché
+ non avrà inserito un numero corretto. */
+
+ //Variabili
+ const accessBtn = document.querySelector('.btn');
+
+ //Ciclo do-whlile
+
+ do{
+    accessBtn.addEventListener('click', function(){
+
+    })
+ }
+
